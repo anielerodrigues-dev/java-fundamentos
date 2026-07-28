ee6bde5da5cd5588c8b427aac54346fd06689613
Aniele Rodrigues <anielerodrigues19@gmail.com>
2026-07-27 23:09:47 -0300

    Add Programa.java with personal information display

---
+++ Aula01/Programa.java
@@ -0,0 +1,19 @@
+public class Programa {
+
+    public static void main(String[] args) {
+        
+
+       String nome = "Aniele";
+       int idade = 26;
+       double altura = 1.61;
+       char inicial = 'A';
+       boolean estudandoJava = true;
+
+    
+       System.out.println("Nome: " + nome);
+       System.out.println("Idade: " + idade);
+       System.out.println("Altura: " + altura);
+       System.out.println("Inicial: " + inicial);
+       System.out.println("Está estudando Java? " + estudandoJava);
+    }
+}
