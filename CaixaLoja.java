dcc38f1071bfe70d69ce86e26beb8fc0e95511a3
Aniele Rodrigues <anielerodrigues19@gmail.com>
2026-07-29 02:18:04 -0300

    Implement cash register functionality in CaixaLoja

---
+++ CaixaLoja.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class CaixaLoja {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+
+        double total = 0;
+
+
+        System.out.println("Valor da compra: ");
+        double valorCompra = scanner.nextDouble();
+
+
+        while (valorCompra != 0 ) {
+        total = total + valorCompra;
+        
+        System.out.print("Valor da compra:");
+        valorCompra = scanner.nextDouble();
+
+
+        }
+
+        System.out.println("Total: R$ " + total);
+        scanner.close();
+
+
+    }
+}
