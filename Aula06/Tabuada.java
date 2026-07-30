f122f2f0d22292d1a8b8e03a28779df05131bc7b
Aniele Rodrigues <anielerodrigues19@gmail.com>
2026-07-30 16:07:05 -0300

    Add Tabuada class to print multiplication table

---
+++ Aula06/Tabuada.java
@@ -0,0 +1,14 @@
+public class Tabuada {
+    public static void main(String[] args) {
+        
+    int tabuada = 8;
+
+    for (int i = 1; i <= 10; i++) {
+
+    System.out.println(tabuada + " x " + i + " = " + (tabuada*i));
+
+    
+        }
+
+    }
+}
