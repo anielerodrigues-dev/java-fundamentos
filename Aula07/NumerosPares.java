7b320de7e6bcae2081df3ef1730f67fecb4327e8
Aniele Rodrigues <anielerodrigues19@gmail.com>
2026-07-31 21:42:35 -0300

    Add NumerosPares class to print even numbers

---
+++ Aula07/NumerosPares.java
@@ -0,0 +1,13 @@
+public class NumerosPares {
+    
+    public static void main(String[] args) {
+        
+    for(int i = 2; i <= 20; i += 2){
+
+    System.out.println(i);
+
+
+
+        }
+    }
+}
