70590a9397060b4652a26d338a2e74e316a2efad
Aniele Rodrigues <anielerodrigues19@gmail.com>
2026-07-31 21:43:41 -0300

    Add SomaPares class to sum even numbers from 2 to 20

---
+++ Aula07/SomaPares.java
@@ -0,0 +1,16 @@
+public class SomaPares {
+    
+    public static void main(String[] args) {
+        
+        int soma = 0;
+       
+        for( int i = 2 ; i <= 20; i += 2 ){
+
+        soma = soma + i; 
+
+        }
+
+        System.out.println(soma);
+        
+    }
+}
