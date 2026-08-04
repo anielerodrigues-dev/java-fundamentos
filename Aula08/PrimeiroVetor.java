47c97d3de41d0dfde6a22ea9c98b3f91d90a0bc0
Aniele Rodrigues <anielerodrigues19@gmail.com>
2026-08-03 22:37:18 -0300

    Add PrimeiroVetor class to print array elements

---
+++ Aula08/PrimeiroVetor.java
@@ -0,0 +1,15 @@
+public class PrimeiroVetor {
+    public static void main(String[] args) {
+        
+        int[] numeros = {10, 20, 30, 40, 50};
+    
+        System.out.println(numeros[0]);
+        System.out.println(numeros[1]);
+        System.out.println(numeros[2]);
+        System.out.println(numeros[3]);
+        System.out.println(numeros[4]);
+    
+    
+    }
+        
+}
